8ccbc164305bdee292226474f383f1feca1ad773
akash bodhale <akash.bodhale@gmail.com>
2021-06-15 00:01:53 +0530

    Model

---
+++ ToDo/Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private static work task = new work(); 
+        [HttpGet("get")]
+        public ActionResult<work> Get()
+        {
+            return BadRequest();
+        }
+
+    }
+}
---
+++ ToDo/Models/work.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class work
+    {
+        public int Id { get; set; } = 1;
+        public string Name { get; set; } = "reading";
+        public bool complete { get; set; } = false;
+    }
+}
